4d53b3cc5587b9595a513edbc362e63878d9b9ea
Paulo Xavier <pxavierf@gmail.com>
2021-02-25 13:43:16 -0300

    condicional para saber o desconto do inss

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.1/exercicio-11.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.1/exercicio-11.js
@@ -3,3 +3,13 @@ let salarioBase;
 let salarioLiquido;
 let descontoInss;
 let descontoIr;
+
+if(salarioBruto <= 1556.94){
+  descontoInss = salarioBruto * 0.08;
+}else if(salarioBruto <= 2594.92){
+  descontoInss = salarioBruto * 0.09;
+}else if(salarioBruto <= 5189.82){
+  descontoInss = salarioBruto * 0.11;
+}else{
+  descontoInss = 570.88;
+}
