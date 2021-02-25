5e4e5f0d01b73a36f6834c59e2ef0c88104fd357
Paulo Xavier <pxavierf@gmail.com>
2021-02-25 16:13:12 -0300

    conclusao programa retorna maior valor de um array

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.2/exercicio-5.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let bigNumber = numbers[0];
+
+for(let index = 0; index < numbers.length; index += 1){
+  if(bigNumber < numbers[index]){
+    bigNumber = numbers[index];
+  }else{
+    bigNumber = bigNumber;
+  }
+}
+
+console.log(bigNumber);
