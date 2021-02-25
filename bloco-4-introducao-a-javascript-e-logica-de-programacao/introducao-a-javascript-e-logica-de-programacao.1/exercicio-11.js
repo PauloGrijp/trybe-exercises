b349057a08a162cbe2bf6cf8ed459c1d00d71d60
Paulo Xavier <pxavierf@gmail.com>
2021-02-25 13:51:28 -0300

    conclusao

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.1/exercicio-11.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.1/exercicio-11.js
@@ -13,3 +13,23 @@ if(salarioBruto <= 1556.94){
 }else{
   descontoInss = 570.88;
 }
+
+salarioBase = salarioBruto - descontoInss;
+
+if(salarioBase <= 1903.98){
+  salarioLiquido = salarioBase;
+}else if(salarioBase <= 2826.65){
+  descontoIr = (salarioBase * 0.075) - 142.80;
+  salarioLiquido = salarioBase - descontoIr;
+}else if(salarioBase <= 3751.05){
+  descontoIr = (salarioBase * 0.15) - 354.80;
+  salarioLiquido = salarioBase - descontoIr;
+}else if(salarioBase <= 4664.68){
+  descontoIr = (salarioBase * 0.225) - 636.13;
+  salarioLiquido = salarioBase - descontoIr;
+}else if(salarioBase > 4664.68){
+  descontoIr = (salarioBase * 0.275) - 869.36;
+  salarioLiquido = salarioBase - descontoIr;
+}
+
+console.log(salarioLiquido);
