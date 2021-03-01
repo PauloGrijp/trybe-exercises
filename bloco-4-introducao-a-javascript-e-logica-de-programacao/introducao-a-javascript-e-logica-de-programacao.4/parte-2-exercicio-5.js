e1610a5faa2abbb8366b79d6bae519501c25e4fc
Paulo Xavier <pxavierf@gmail.com>
2021-03-01 18:00:43 -0300

    conclusao de programa que retorna o numero que mais se repete em um array

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.4/parte-2-exercicio-5.js
@@ -0,0 +1,29 @@
+
+function moreRepeated(numbers) {
+
+  let num = {};
+
+  for(let index = 0; index < numbers.length; index += 1){
+    if(num[numbers[index]] === undefined){
+      num[numbers[index]] = 1;
+    }else{
+      num[numbers[index]] += 1
+    }
+  }
+
+  let count = num[Object.keys(num)[0]]
+  let finalNum = Object.keys(num)[0];
+  for(let key in num){
+    if(count < num[key]){
+      count = num[key]
+      finalNum = key
+    }else{
+      count = count
+      finalNum = finalNum;
+    }
+  }  
+  return finalNum;
+  
+}
+
+console.log(moreRepeated([2, 3, 2, 5, 8, 2, 3])); //2
