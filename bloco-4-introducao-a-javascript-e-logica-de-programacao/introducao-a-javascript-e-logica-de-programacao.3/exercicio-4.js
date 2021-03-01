4b809d6a078a446f9b5392696e826f7fb1183345
Paulo Xavier <pxavierf@gmail.com>
2021-03-01 12:52:11 -0300

    iniciando exercicio-4

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.3/exercicio-4.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.3/exercicio-4.js
@@ -1,6 +1,9 @@
-let n = 7;
+let n = 5;
 let asterisk = '';
 let tempN = n;
+let base = n;
+
+
 
 
 for(let index = 1; index <= n; index += 1){
@@ -8,16 +11,21 @@ for(let index = 1; index <= n; index += 1){
   asterisk = '';
 
   if(index < n){
-    for(let j = 1; j <= tempN; j += 1){   
-      asterisk += '$';
+
+    for(let j = 1; j <= tempN; j += 1){
+      
+      if(j == tempN - (tempN - 1) || j == tempN){
+        asterisk += ' ';
+      } else{
+        asterisk += '*';
+      }    
     }
   }else{
-    for(let j = 1; j <= tempN; j += 1){   
+    for(let j = 1; j <= base; j += 1){   
       asterisk += '*';
     }
   }
-
-
+  
   console.log(asterisk); 
   n -= 1;
 } 
