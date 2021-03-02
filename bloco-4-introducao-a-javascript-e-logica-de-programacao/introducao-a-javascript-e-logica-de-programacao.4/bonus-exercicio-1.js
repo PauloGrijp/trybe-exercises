c71a5c5944888ad5916827a56b5796af7292b476
Paulo Xavier <pxavierf@gmail.com>
2021-03-01 21:16:17 -0300

    conclusao de programa que converte algorismo romano em numero cardial

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.4/bonus-exercicio-1.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.4/bonus-exercicio-1.js
@@ -1,6 +1,6 @@
 
 function convertsRomAlgo(romanAlgorithm){
-
+  let roman = romanAlgorithm.toUpperCase();
   let romaAlgo = {
     I: 1,
     V: 5,
@@ -9,22 +9,23 @@ function convertsRomAlgo(romanAlgorithm){
     C: 100,
     D: 500,
     M: 1000,
-  };
+  };  
   
   let sum = 0;
-  let num = [];
-
-  for (let index = 0; index < romanAlgorithm.length; index += 1){   
-    for(let key in romaAlgo){
-      if(romanAlgorithm[index] == key){
-        
-      }
-    }
-  }
-
   
+
+  for (let index = 0; index < roman.length; index += 1){   
+    let atual = roman[index];
+    let next = roman[index + 1];   
+
+      if(romaAlgo[next] > romaAlgo[atual]){
+        sum -= romaAlgo[atual];
+      }else{
+        sum += romaAlgo[atual];
+      }
+  }   
   return sum
   
 }
 
-console.log(convertsRomAlgo('IX'))
+console.log(convertsRomAlgo('X'))
