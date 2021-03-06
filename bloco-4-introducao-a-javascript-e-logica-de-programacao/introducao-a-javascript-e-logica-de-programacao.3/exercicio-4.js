52879e50ce6b33748999b3342926029c09c4ecb7
Paulo Xavier <pxavierf@gmail.com>
2021-03-06 11:31:06 -0300

    concluindo exercicio-4

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.3/exercicio-4.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.3/exercicio-4.js
@@ -1,35 +1,22 @@
+
 let n = 5;
-let asterisk = '';
-let tempN = n;
-let base = n;
+let symbol = '*';
+let inputLine = '';
 
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
 
-
-
-for(let index = 1; index <= n; index += 1){
-
-  asterisk = '';
-
-  if(index < n){
-
-    for(let j = 1; j <= tempN; j += 1){
-      
-      if(j == tempN - (tempN - 1) || j == tempN){
-        asterisk += ' ';
-      } else{
-        asterisk += '*';
-      }    
-    }
-  }else{
-    for(let j = 1; j <= base; j += 1){   
-      asterisk += '*';
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
     }
   }
-  
-  console.log(asterisk); 
-  n -= 1;
-} 
-
-
-
-
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
