20fe2ac57ad6e605491459cbd3f57950af01e0c9
Paulo Xavier <pxavierf@gmail.com>
2021-03-06 11:41:45 -0300

    commit final

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.4/bonus-exercicio-2.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/introducao-a-javascript-e-logica-de-programacao.4/bonus-exercicio-2.js
@@ -1,56 +1,25 @@
 function add(num1, num2) {
-  
-  let stringNum1 = num1.toString();
-  let stringNum2 = num2.toString();
+  const arrNum1 = num1.toString().split("").reverse();
+  const arrNum2 = num2.toString().split("").reverse();
+  let lessArr;
+  let bigArr;
+  const output = [];
 
-  let reverseNum1 = stringNum1.split('').reverse();
-  let reverseNum2 = stringNum2.split('').reverse();
+  if (arrNum1.length > arrNum2.length) {
+    bigArr = arrNum1;
+    lessArr = arrNum2;
+  } else {
+    bigArr = arrNum2;
+    lessArr = arrNum1;
+  }
 
-  let sum = [];
-
-  for(let indexNum1 = 0; indexNum1 < reverseNum1.length; indexNum1 += 1){
-    for(let indexNum2 = 0; indexNum2 < reverseNum2.length; indexNum2 += 1){
-      if(reverseNum1.length == reverseNum2.length){
-        if(indexNum1 == indexNum2){
-          let convertNum1 = parseInt(reverseNum1[indexNum1]);
-          let convertNum2 = parseInt(reverseNum2[indexNum2]);
-          let sumNumbers = convertNum1 + convertNum2;       
-          let numberToString = sumNumbers.toString()
-          sum.push(numberToString)
-        }
-      }else{
-        if(reverseNum1.length > reverseNum2.legth){
-          let diferencaReverse = reverseNum1.length - reverseNum2.legth;
-          for(let i = 0; i < diferencaReverse; i++){
-            reverseNum2.push('0')
-          }
-          let convertNum1 = parseInt(reverseNum1[indexNum1]);
-          let convertNum2 = parseInt(reverseNum2[indexNum2]);
-          let sumNumbers = convertNum1 + convertNum2;       
-          let numberToString = sumNumbers.toString()
-          sum.push(numberToString)
-
-        }else{
-          let diferencaReverse = reverseNum2.length - reverseNum1.legth;
-          for(let i = 0; i < diferencaReverse; i++){
-            reverseNum1.unshift('0')
-          }
-          let convertNum1 = parseInt(reverseNum1[indexNum1]);
-          let convertNum2 = parseInt(reverseNum2[indexNum2]);
-          let sumNumbers = convertNum1 + convertNum2;       
-          let numberToString = sumNumbers.toString()
-          sum.push(numberToString)
-        }
-      }
+  for (let i = 0; i < bigArr.length; i += 1) {
+    if (lessArr[i] !== undefined) {
+      output[i] = parseInt(bigArr[i]) + parseInt(lessArr[i]);
+    } else {
+      output[i] = parseInt(bigArr[i]);
     }
   }
 
-  sum =  sum.reverse().join('');
-  
-  return sum; 
-
-  
+  return parseInt(output.reverse().join(""));
 }
-
-console.log(add(1, 18))
-
