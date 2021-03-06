f051935368495e5306a2c8697224b05b6b09f971
Paulo Xavier <pxavierf@gmail.com>
2021-03-06 12:35:07 -0300

    criando lista com os dias do mes

--- bloco-5-javascript-dom-eventos-e-web-storage/5.3-eventos/script.js
+++ bloco-5-javascript-dom-eventos-e-web-storage/5.3-eventos/script.js
@@ -13,4 +13,15 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+function days(){
+  const days_ul = document.getElementById('days');
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+  for(let index of dezDaysList){
+    const listDays_li = document.createElement('li');
+    listDays_li.innerText = index;
+    days_ul.appendChild(listDays_li);
+  } 
+}
+
+days()
