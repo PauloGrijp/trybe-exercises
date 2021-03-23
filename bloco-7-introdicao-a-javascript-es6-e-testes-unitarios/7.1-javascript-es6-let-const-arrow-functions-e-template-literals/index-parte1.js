5c4715c618785771d077136556a5407cd94bd233
Paulo Xavier <pxavierf@gmail.com>
2021-03-23 15:59:47 -0300

    parte 1

---
+++ bloco-7-introdicao-a-javascript-es6-e-testes-unitarios/7.1-javascript-es6-let-const-arrow-functions-e-template-literals/index-parte1.js
@@ -0,0 +1,18 @@
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+let order = array => {  
+  array.sort((a,b) => a -b);
+  console.log(array)
+}
+order([13, 3, 4, 10, 7, 2])
