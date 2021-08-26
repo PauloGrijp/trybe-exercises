a1c7c8d9d8b09006466238a535cf196a12e7b288
Paulo Xavier <pxavierf@gmail.com>
2021-08-26 11:58:48 -0300

    calculator imc

--- bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/imc.js
+++ bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/imc.js
@@ -1,2 +1,8 @@
 const readline = require('readline-sync');
 
+const peso = readline.questionFloat('Qual o seu peso? ');
+const altura = readline.questionFloat('Qual a sua altura em metros? ');
+
+const resultado = peso / Math.pow(altura, 2);
+
+console.log(resultado)
