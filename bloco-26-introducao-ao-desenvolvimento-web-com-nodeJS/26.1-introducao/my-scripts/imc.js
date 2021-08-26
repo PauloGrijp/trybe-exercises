18c8b8d56bf909d8d38a36a6e017398129bbd6e6
Paulo Xavier <pxavierf@gmail.com>
2021-08-26 12:04:07 -0300

    imc - conditions situation

--- bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/imc.js
+++ bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/imc.js
@@ -1,8 +1,40 @@
 const readline = require('readline-sync');
 
-const peso = readline.questionFloat('Qual o seu peso? ');
-const altura = readline.questionFloat('Qual a sua altura em metros? ');
+function imc() {
+  const peso = readline.questionFloat('Qual o seu peso? ');
+  const altura = readline.questionFloat('Qual a sua altura em metros? ');
+  
+  const resultado = peso / Math.pow(altura, 2);
 
-const resultado = peso / Math.pow(altura, 2);
+  console.log(resultado.toFixed(2));
 
-console.log(resultado)
+  if (resultado < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
+  }
+
+  if (resultado >= 18.5 && resultado < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+
+  if (resultado >= 25 && resultado < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+
+  if (resultado >= 30 && resultado < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+
+  if (resultado >= 35 && resultado < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }
+
+  console.log('Situação: Obesidade graus III e IV');
+
+}  
+
+imc();
