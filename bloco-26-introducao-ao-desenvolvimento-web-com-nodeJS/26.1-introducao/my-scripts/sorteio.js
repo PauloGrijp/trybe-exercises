218ef3b650d22c2dbb0cd088e6a07135312de88d
Paulo Xavier <pxavierf@gmail.com>
2021-08-26 12:23:59 -0300

    raffle game

--- bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/sorteio.js
+++ bloco-26-introducao-ao-desenvolvimento-web-com-nodeJS/26.1-introducao/my-scripts/sorteio.js
@@ -1,2 +1,22 @@
 const readline = require('readline-sync');
 
+function raffle() {
+
+  const choice = readline.questionInt('Insira um numero entre 0 e 10: ');
+
+  const random = Math.floor(Math.random() * 11);
+
+  console.log(random)
+  if(random === choice) {
+    console.log("Parabéns, número correto!")
+  } else {
+    console.log(`Opa, não foi dessa vez. O número era ${random}`)
+  }
+
+  const playAgain = readline.question('Deseja jogar novamente? ');
+
+  if(playAgain === 'sim') raffle()
+
+}
+
+raffle()
